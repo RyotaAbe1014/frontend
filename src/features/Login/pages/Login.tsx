2a5f797c4e84a3ae196ae2afbbbd4b364b286c86
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-04-30 19:25:37 +0900

    feat: tailwind導入

---
+++ src/features/Login/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Login: React.FC = () => {
+  return (
+    // ログイン画面
+    <div>
+      <h1>ログイン</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.tsx'], // またはjsx, ts, jsなどの拡張子に応じて変更
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
