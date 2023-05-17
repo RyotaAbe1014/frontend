3fad33138a9d80fba4718e96c0917fd7475ff4ce
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-05-17 23:26:41 +0900

    :bug: ナビバーの表示が2重になる問題を修正

--- src/common/components/_organisms/SideMenu.tsx
+++ src/common/components/_organisms/SideMenu.tsx
@@ -5,13 +5,13 @@ const SideMenu: React.FC = () => {
     <aside className="h-screen w-2/12 bg-white p-6 shadow">
       <div className="menu">
         <p className="mt-2 rounded-md px-4 py-2 text-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50">
-          <button>プロダクトバックログ管理</button>
+          <button>プロダクトバックログ</button>
         </p>
         <p className="rounded-md px-4 py-2 text-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50">
-          <button>スプリント管理</button>
+          <button>スプリント</button>
         </p>
         <p className="mt-2 rounded-md px-4 py-2 text-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50">
-          <button>スプリントバックログ管理</button>
+          <button>スプリントバックログ</button>
         </p>
         <p className="mt-2 rounded-md px-4 py-2 text-indigo-600 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50">
           <button>ユーザ管理</button>
