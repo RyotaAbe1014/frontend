b6ae93c1761d30929850befbab0d360526b33349
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-06-01 01:47:11 +0900

    ⚗️ feat: dnd-kitのsample

--- src/features/Scrum/pages/SprintBacklogList.tsx
+++ src/features/Scrum/pages/SprintBacklogList.tsx
@@ -1,12 +1,83 @@
-import React from 'react'
-import { DefaultLayout } from '../../../common/_components/_templates/DefaultLayout'
+import React, { useState } from 'react';
+import { DefaultLayout } from '../../../common/_components/_templates/DefaultLayout';
+
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+function SortableItem(props: any) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className='bg-white p-4 rounded-md shadow-md max-w-xs'>
+      Item {props.id}
+    </div>
+  );
+}
 
 export const SprintBacklogList: React.FC = () => {
+  const [items, setItems] = useState(["1", "2", "3"]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
-    <>
-      <DefaultLayout>
-        
-      </DefaultLayout>
-    </>
-  )
+    <DefaultLayout>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={items}
+          strategy={verticalListSortingStrategy}
+        >
+          <div className='space-y-4  bg-white p-4 rounded-md shadow-md max-w-xs'>
+          {items.map(id => <SortableItem key={id} id={id} />)}
+          </div>
+        </SortableContext>
+      </DndContext>
+    </DefaultLayout>
+  );
 }
