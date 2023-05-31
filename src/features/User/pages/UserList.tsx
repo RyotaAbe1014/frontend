40af472827cb48ed0005f24a3af6f64408dbe9fd
shiki <akkt1102@gmail.com>
2023-06-01 00:03:50 +0900

    isStaffがfalseの場合にurl直打ちでusersを開くとtopに戻るように

--- src/features/User/pages/UserList.tsx
+++ src/features/User/pages/UserList.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { DefaultLayout } from '../../../common/_components/_templates/DefaultLayout'
 import UserListTable from '../organisms/UserListTable'
 import { UserProvider } from '../../../services/providers/user/UserProvider'
 
+import { User } from '../../../types/user/user';
+import { useNavigate } from 'react-router-dom';
+
 export const UserList: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const storage_user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '{}') : {};
+  const user: User = storage_user.user;
+
+  useEffect(() => {
+    if (!user.isStaff) {
+      navigate("/")
+    }
+  }, [])
 
   return (
     <>
